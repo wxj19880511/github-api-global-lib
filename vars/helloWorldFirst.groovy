2bb887c07da2a6816d268084cbe7577484ca0d4f
Kevin Wang <354798650@qq.com>
2022-11-01 11:00:20 +0800

    Update helloWorldFirst.groovy

--- vars/helloWorldFirst.groovy
+++ vars/helloWorldFirst.groovy
@@ -1,3 +1,3 @@
-def call(String name, String dayOfWeek) {
-  sh "echo hello ${name}. day of week is ${dayOfWeek} "
+def call(Map config=[:]) {
+  sh "echo hello ${config.name}. day of week is ${config.dayOfWeek} "
 }
